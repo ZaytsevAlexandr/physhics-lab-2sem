d339bac705b144d9bf7bc0eea0290d305342fe90
Zaytsev Aleksandr <145544514+ZaytsevAlexandr@users.noreply.github.com>
2024-03-25 16:41:35 +0300

    Add files via upload

--- 2.1.6/main.py
+++ 2.1.6/main.py
@@ -1,11 +1,12 @@
-import numpy
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def fit_line(x, y):
     A = np.vstack([x, np.ones(len(x))]).T
     k, b = np.linalg.lstsq(A, y, rcond=None)[0]
     return k, b
+
 
 def read_points(filename):
     with open(filename, 'r') as file:
@@ -14,27 +15,48 @@ def read_points(filename):
         y = [float(line.split()[1]) for line in lines]
     return x, y
 
+
 colors = ['red', 'green', 'blue', 'purple']
 
 plt.figure(figsize=(8, 6))
-plt.grid(True)
+plt.grid(which="both")
+plt.minorticks_on()
+plt.tick_params(which="minor", bottom=True, left=True)
 
 T = [20, 35, 45, 55]
+mu_error = []
 
 for i in range(4):
     x, y = read_points(f'data_{i+1}.txt')
     k, b = fit_line(x, y)
+    sr_x2 = 0
+    sr_x = 0
+    sr_y = 0
+    sr_y2 = 0
+    for e in range(6):
+        sr_x2 = sr_x2 + x[e] * x[e]
+        sr_x = sr_x + x[e]
+    sr_x2 = sr_x2 / 6
+    sr_x = sr_x / 6
+    for h in range(6):
+        sr_y2 = sr_y2 + y[h] * y[h]
+        sr_y = sr_y + y[h]
+    sr_y2 = sr_y2 / 6
+    sr_y = sr_y / 6
+    error_k = (1 / (6**0.5)) * (((sr_y2 - sr_y*sr_y) / (sr_x2 - sr_x*sr_x) - k**2)**0.5)
     c = []
     for j in range(len(x)):
         c.append(k * x[j] + b)
-    plt.plot(x, c, color=colors[i], linewidth=2, label=f'T = {T[i]}°C, μ = {round(k, 2)} К/бар')
-    plt.errorbar(x, y, xerr=0.01, yerr=0.01, fmt='o', color=colors[i])
+    plt.plot(x, c, color=colors[i], linestyle='dashed', linewidth=2, label=f'T = {T[i]}°C, μ = {round(k,2)} $\pm$ {round(error_k,2)} K/бар')
+    plt.errorbar(x, y, xerr=0.1, yerr=0.01, fmt='.', color=colors[i], markersize='2', capsize=1, elinewidth=1)
 
 plt.legend()
+# plt.text(0.005, 0.72, f'Угловой коэффициент прямой k: {round(k, 3)}')
+# plt.text(0.005, 0.71, f'Свободный член m: {round(b, 2)}')
 plt.xlabel('ΔP, бар')
-plt.ylabel('ΔT, °C')
-plt.title('ΔT от ΔР')
-plt.xlim(1.3, 4.2)
-plt.ylim(0, 3.2)
+plt.ylabel('ΔT, K')
+plt.title('ΔT(ΔP)')
+plt.xlim(1.38, 4.2)
+plt.ylim(0, 3.1)
 
 plt.show()
---
+++ 2.1.6/second.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def fit_line(x, y):
+    A = np.vstack([x, np.ones(len(x))]).T
+    k, b = np.linalg.lstsq(A, y, rcond=None)[0]
+    return k, b
+
+
+def read_points(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        x = [float(line.split()[0]) for line in lines]
+        y = [float(line.split()[1]) for line in lines]
+    return x, y
+
+
+plt.figure(figsize=(8, 6))
+plt.grid(which="both")
+plt.minorticks_on()
+plt.tick_params(which="minor", bottom=True, left=True)
+
+x, y = read_points('data_0.txt')
+k, b = fit_line(x, y)
+sr_x2 = 0
+sr_x = 0
+sr_y = 0
+sr_y2 = 0
+for e in range(4):
+    sr_x2 = sr_x2 + x[e] * x[e]
+    sr_x = sr_x + x[e]
+sr_x2 = sr_x2 / 4
+sr_x = sr_x / 4
+for h in range(4):
+    sr_y2 = sr_y2 + y[h] * y[h]
+    sr_y = sr_y + y[h]
+sr_y2 = sr_y2 / 4
+sr_y = sr_y / 4
+error_k = (1 / (4 ** 0.5)) * (abs((sr_y2 - sr_y * sr_y) / (sr_x2 - sr_x * sr_x) - k ** 2) ** 0.5)
+error_m = error_k * ((sr_x2 - sr_x * sr_x)**0.5)
+c = []
+for j in range(len(x)):
+    c.append(k * x[j] + b)
+plt.plot(x, c, color='blue', linestyle='solid', linewidth=2)
+errors = [0.01, 0.06, 0.02, 0.02]
+for e in range(4):
+    plt.errorbar(x[e], y[e], xerr=0, yerr=errors[e], fmt='o', color='red', markersize='4', capsize=2, elinewidth=0.3)
+
+# plt.legend()
+plt.text(0.00305, 0.7, f'Угловой коэффициент прямой k: {round(k, 3)} $\pm$ {round(error_k,2)}')
+plt.text(0.00305, 0.65, f'Свободный член m: {round(b, 3)} $\pm$ {round(error_m,2)}')
+plt.xlabel('1/T, K$^-$$^1$')
+plt.ylabel('$\mu$, K/бар')
+plt.title('$\mu$(T$^-$$^1$)')
+plt.xlim(0.003, 0.0035)
+plt.ylim(0.45, 1.35)
+
+plt.show()
